bec4841324fd38afcb924c6f0c6c7271df2892f1
Srinivasan <rm.srini@gmail.com>
2022-07-17 19:00:43 +0530

    BIFI-1 common files

---
+++ common/access_db/database.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+
+class Database(ABC):
+    def __int__(self, db_source):
+        self.db_source = db_source
+
+    @abstractmethod
+    def get_connection_str(self):
+        self.db_source
