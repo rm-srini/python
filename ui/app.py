7eabf44ad386bc5bb98bd40cea64b56903548dc7
Srinivasan <rm.srini@gmail.com>
2023-08-05 23:53:31 +0530

    BIFI-1 common files

--- ui/app.py
+++ ui/app.py
@@ -1,28 +1,16 @@
 import streamlit as st
-import pandas as pd
-import altair as alt
-
-data = {
-    'Category': ['Category 1', 'Category 2', 'Category 3'],
-    'Value': [30, 40, 50]
-}
-
-df = pd.DataFrame(data)
 
 
-def create_bar_chart(df):
-    bar_chart = alt.Chart(df).mark_bar().encode(
-        x='Category:N',
-        y='Value:Q',
-        tooltip=['Category', 'Value']
-    ).properties(
-        width=400,
-        height=300
-    )
-    return bar_chart
+def main():
+    st.title("Simple Greeting App")
+
+    # Get user input for name
+    name = st.text_input("Enter your name:")
+
+    # Display the greeting message
+    if name:
+        st.write(f"Hello, {name}!")
 
 
-st.title('Bar Chart Example')
-st.write(df)
-bar_chart = create_bar_chart(df)
-st.altair_chart(bar_chart)
+if __name__ == "__main__":
+    main()
