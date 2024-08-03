89208a536fdbc8e08eabf38f010de98c071aac26
Srinivasan <rm.srini@gmail.com>
2024-08-03 10:14:26 +0530

    BIFI-1 common files

---
+++ project_stock_filter/config/config.py
@@ -0,0 +1,10 @@
+stock_attribute = ['regularMarketPreviousClose', 'regularMarketDayLow', 'regularMarketDayHigh', 'beta',
+                   'trailingPE', 'forwardPE', 'volume', 'marketCap', 'fiftyTwoWeekLow', 'regularMarketOpen',
+                   'fiftyTwoWeekHigh', 'fiveYearAvgDividendYield', 'priceToSalesTrailing12Months',
+                   'fiftyDayAverage', 'twoHundredDayAverage', 'profitMargins', 'floatShares',
+                   'sharesOutstanding', 'bookValue', 'priceToBook', 'earningsQuarterlyGrowth',
+                   'targetHighPrice', 'targetLowPrice', 'targetMeanPrice', 'targetMedianPrice',
+                   'recommendationMean', 'totalDebt', 'returnOnEquity', 'earningsGrowth',
+                   'revenueGrowth', 'trailingEps', 'forwardEps', 'trailingPegRatio']
+
+stock_list = []
